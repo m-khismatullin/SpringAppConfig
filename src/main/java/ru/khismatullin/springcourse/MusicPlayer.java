5f18f9cb9101652b62def0a9f3be338f0823cf70
m-khismatullin <m.khismatullin@yandex.ru>
2023-09-07 17:40:47 +0500

    Spring with Config Class (manual bean construction)

--- src/main/java/ru/khismatullin/springcourse/MusicPlayer.java
+++ src/main/java/ru/khismatullin/springcourse/MusicPlayer.java
@@ -1,22 +1,20 @@
 package ru.khismatullin.springcourse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MusicPlayer {
-    @Autowired
-    @Qualifier("classicalMusic")
     private Music music1;
-    @Autowired
-    @Qualifier("jazzMusic")
     private Music music2;
     @Value("${musicPlayer.name}")
     private String name;
     @Value("${musicPlayer.volume}")
     private int volume;
+
+    public MusicPlayer(Music music1, Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
+    }
 
     public String playMusic() {
         return "Playing: " + music1.getSong() + ", " + music2.getSong();
